d4025fafe6cf15ff668528a9e929df89760c2d0a
EKR <ekr@rtfm.com>
2025-04-05 09:14:45 -0700

    Added

---
+++ static/units.js
@@ -0,0 +1,40 @@
+function Units() {
+  const settings = Settings();
+
+  function metric() {
+    return settings.get("units") === "metric";
+  }
+
+  function distanceValue(distance) {
+    return metric() ? distance / 1000 : (distance * 0.62) / 1000;
+  }
+
+  function distanceUnits() {
+    return metric() ? "km" : "mi";
+  }
+
+  function elevationValue(elevation) {
+    return metric() ? elevation : elevation * 3.2808;
+  }
+
+  function elevationUnits() {
+    return metric() ? "m" : "ft";
+  }
+
+  function distanceDiffValue(distance) {
+    return metric() ? distance : distance * 3.2808;
+  }
+
+  function distanceDiffUnits() {
+    return metric() ? "m" : "ft";
+  }
+
+  return {
+    distanceValue,
+    distanceUnits,
+    elevationValue,
+    elevationUnits,
+    distanceDiffValue,
+    distanceDiffUnits,
+  };
+}
